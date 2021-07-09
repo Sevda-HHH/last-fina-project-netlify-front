c8b34b4b7ca961d486db0bafcce50615daa4c382
Sevda-HHH <SevdaMh@code.edu.az>
2021-07-10 02:29:08 +0400

    Bezdim

--- src/components/Appointment/AddAppointmentForm.tsx
+++ src/components/Appointment/AddAppointmentForm.tsx
@@ -68,7 +68,7 @@ export const AddAppointmentForm = () => {
                                 await appointments.map(async (appoint: IAppointment) => {
                                     console.log(Math.abs(moment(values.date).diff(appoint.date)))
 
-                                    if (Math.abs(moment(values.date).diff(appoint.date)) <= 16000000) {
+                                    if (Math.abs(moment(values.date).diff(appoint.date)) <= 14000000) {
                                         console.log("daxil oldu 3")
                                         count = count + 1
                                     }
