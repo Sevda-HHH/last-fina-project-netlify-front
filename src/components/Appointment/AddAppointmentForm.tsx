4a1622dbc44012b05374f3ba32d19b0a2ee00f8b
Sevda-HHH <SevdaMh@code.edu.az>
2021-07-10 03:34:09 +0400

    sda

--- src/components/Appointment/AddAppointmentForm.tsx
+++ src/components/Appointment/AddAppointmentForm.tsx
@@ -66,8 +66,8 @@ export const AddAppointmentForm = () => {
                                 warnHasPast!.classList.add("d-none")
                                 let count = 0
                                 await appointments.map(async (appoint: IAppointment) => {
-                                    console.log("value date ", moment(values.date))
-                                    console.log("appoint date ", moment(appoint.date))
+                                    console.log("value date ", moment(values.date).format("YYYY-MM-DDTHH:mm"))
+                                    console.log("appoint date ", moment(appoint.date).format("YYYY-MM-DDTHH:mm"))
                                     console.log(moment.duration(moment(values.date).diff(moment(appoint.date).format("YYYY-MM-DDTHH:mm"))).asMinutes())
                                     if (Math.abs(moment(values.date).diff(moment(appoint.date).format("YYYY-MM-DDTHH:mm"))) <= 12600000) {
                                         console.log("daxil oldu 3")
